caa27808457bea10ad2c1d445c8810e5f32309fa
Tom Harbert <tomh@tomini.local>
2017-11-26 01:14:49 -0800

    working proof of concept

---
+++ config.py
@@ -0,0 +1,8 @@
+
+ACCOUNT_PASSWD = '...'
+CONTRACT_ADDR = '...'
+IPC_ENDPOINT = '/Users/.../Library/Ethereum/netchain/geth.ipc'
+LOGFILE = '.../netchain/netchain.log'
+USER = '...'
+VERSION = '0.01-POC'
+WORKING_DIR = '.../netchain/'
